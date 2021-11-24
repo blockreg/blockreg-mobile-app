523616be656763109c0dc185dd598799f95a3c94
Seamus James <seamus@blockreg.io>
2021-11-23 18:56:40 -0800

    Installed react-native-qrcode-scanner

--- src/screens/ScanScreen.tsx
+++ src/screens/ScanScreen.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import ScreenContainer from './ScreenContainer';
-import {View, Text} from 'react-native';
+import {View, Text, Alert} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
 
 const ScanScreen: React.FC<{}> = () => {
+
+	const onSuccess = () => {
+		Alert.alert("It scanned!");	
+	};
+
 	return (
 		<ScreenContainer>
-			<Text>Scan Screen</Text>
+			<QRCodeScanner
+				onRead={onSuccess}
+			/>
 		</ScreenContainer>
 	);
 }
